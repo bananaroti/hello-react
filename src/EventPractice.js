4f1bf62e1a45075cd8ad7f57d36e9898fe2172d2
bananaroti <gh3298@gmail.com>
2021-12-09 16:12:53 +0900

    Update EventPractice.js

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,29 +2,41 @@ import React from "react";
 import { useState } from "react/cjs/react.development";
 
 const EventPractice = () => {
+  const [username,setUsername] = useState('');
   const [message, setMessage] = useState('');
 
-  function handleChange(e) {
-    setMessage(e.target.value);
-  }
-
-  function handleClick() {
-    alert(message);
+  const onChangeUsername = e => setUsername(e.target.value);
+  const onChangeMessage = e => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username+': '+message);
+    setUsername('');
     setMessage('');
   }
+  const onKeyPress = e => {
+    if(e.key === 'Enter'){
+      onClick();
+    }
+  }
+
   return (
     <div>
       <h1>이벤트 연습</h1>
       <input
         type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUsername}
+      ></input>
+      <input
+        type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={(e) => {
-          handleChange(e);
-        }}
+        onChange={onChangeMessage}
+        onKeyPress={onKeyPress}
       ></input>
-      <button onClick={handleClick}>확인</button>
+      <button onClick={onClick}>확인</button>
     </div>
   );
 };
