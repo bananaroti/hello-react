c08bcb7fc51748738580456ac9fcca21102fe88d
bananaroti <gh3298@gmail.com>
2021-12-10 13:33:10 +0900

    Update EventPractice.js

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,16 +2,26 @@ import React from "react";
 import { useState } from "react/cjs/react.development";
 
 const EventPractice = () => {
-  const [username,setUsername] = useState('');
-  const [message, setMessage] = useState('');
+  const [form, setForm] = useState({
+    username: '',
+    message: '',
+  });
+  const { username, message } = form;
+  const onChange = e => {
+    const nextform = {
+      ...form,
+      [e.target.name]: e.target.value
+    }
+    setForm(nextform);
+  }
 
-  // e = SyntheticEvent , onChange이벤트 발생기시 e.target.value를 기록
-  const onChangeUsername = e => setUsername(e.target.value);
-  const onChangeMessage = e => setMessage(e.target.value);
+
   const onClick = () => {
     alert(username+': '+message);
-    setUsername('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: '',
+    })
   }
   const onKeyPress = e => {
     if(e.key === 'Enter'){
@@ -27,14 +37,14 @@ const EventPractice = () => {
         name="username"
         placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       ></input>
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       ></input>
       <button onClick={onClick}>확인</button>
