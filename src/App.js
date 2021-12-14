977cea489d344fc3599852c4472809e0db5d1695
bananaroti <gh3298@gmail.com>
2021-12-14 11:17:05 +0900

    ref: DOM에 이름 달기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { useRef } from "react";
+import React, { Component } from "react";
 import ScrollBox from "./ScrollBox";
 // import EventPractice from "./EventPractice";
 // import MyComponent from "./MyComponent";
 // import Say from "./Say";
 // import ValidationSample from "./ValidationSample";
 
-const App = () => {
-  const refScrollBox = useRef(refBox);
-  return (
-    <div>
-      {/* <MyComponent name={"React"} favoriteNumber={13}>리액트</MyComponent> */}
-      {/* <Say></Say> */}
-      {/* <EventPractice></EventPractice> */}
-      {/* <ValidationSample></ValidationSample> */}
-      <ScrollBox ref={refScrollBox}></ScrollBox>
-      <button onClick={()=>refScrollBox.scrollToBottom()}>Button</button>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <MyComponent name={"React"} favoriteNumber={13}>리액트</MyComponent> */}
+        {/* <Say></Say> */}
+        {/* <EventPractice></EventPractice> */}
+        {/* <ValidationSample></ValidationSample> */}
+        <ScrollBox ref={ref=>{this.scrollBox=ref}}></ScrollBox>
+        <button onClick={()=>{this.scrollBox.scrollToTop()}}>맨 위로</button>
+        <button onClick={()=>{this.scrollBox.scrollToBottom()}}>맨 밑으로</button>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/ScrollBox.js
+++ src/ScrollBox.js
@@ -1,29 +1,42 @@
-import React, { useRef } from "react";
+import React, { Component } from "react";
 
-const ScrollBox = () => {
-  const refBox = React.createRef();
-  const scrollToBottom = () => {
-    const { scrollHeight, clientHeight } = refBox;
-    refBox.scrollTop = scrollHeight - clientHeight;
-  };
-  const style = {
-    border: "1px solid black",
-    height: "300px",
-    width: "300px",
-    overflow: "auto",
-    position: "relative",
-  };
-  const innerStyle = {
-    width: "100%",
-    height: "650px",
-    background: "linear-gradient(white, black)",
-  };
+class ScrollBox extends Component {
 
-  return (
-    <div style={style} ref={refBox}>
-      <div style={innerStyle}></div>
-    </div>
-  );
-};
+  scrollToTop = () => {
+    const {scrollHeight, clientHeight} = this.box;
+    /*  비구조화 할당 문법
+        아래 코드와 같은 의미
+        const scrollHeight = this.box.scrollHeight;
+        const clientHeight = this.box.clientHeight;
+    */
+    this.box.scrollTop = clientHeight - scrollHeight;
+  }
+
+  scrollToBottom = () => {
+    const {scrollHeight, clientHeight} = this.box;
+    this.box.scrollTop = scrollHeight - clientHeight;
+  }
+
+  render() {
+    const style = {
+      border: "1px solid black",
+      height: "300px",
+      width: "300px",
+      overflow: "auto",
+      position: "relative",
+    };
+    const innerStyle = {
+      width: "100%",
+      height: "650px",
+      background: "linear-gradient(white, black)",
+    };
+
+    return (
+      <div style={style} ref={ref=> {this.box=ref}}>
+        <div style={innerStyle}></div>
+      </div>
+    );
+  }
+}
 
 export default ScrollBox;
