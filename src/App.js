ad427424048e8c304c131818a35cd7001b23755f
bananaroti <gh3298@gmail.com>
2021-12-16 16:35:10 +0900

    React-Hook
    새로운 폴더, 프로젝트를 React-Hooks에 만듦

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import ScrollBox from "./ScrollBox";
 // import IterationSample from "./IterationSample";
+import ErrorBoundary from "./ErrorBoundary";
 import LifeCycleSample from "./LifeCycleSample";
 import React, { Component } from "react";
 
@@ -8,7 +9,20 @@ import React, { Component } from "react";
 // import Say from "./Say";
 // import ValidationSample from "./ValidationSample";
 
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
   render() {
     return (
       <div>
@@ -16,9 +30,12 @@ class App extends Component {
         {/* <Say></Say> */}
         {/* <EventPractice></EventPractice> */}
         {/* <ValidationSample></ValidationSample> */}
-        {/* <ScrollBoxref={(ref) => {this.scrollBox = ref;}}        ></ScrollBoxref=> */}
+        {/* <ScrollBoxref={(ref) => {this.scrollBox = ref;}}></ScrollBoxref=> */}
         {/* <IterationSample></IterationSample> */}
-        <LifeCycleSample></LifeCycleSample>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}></LifeCycleSample>
+        </ErrorBoundary>
       </div>
     );
   }
